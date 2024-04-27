dc2b87ba3b1d35617574c1ee7347aae20374dfa3
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-27 07:50:45 +0300

    no commit message given

---
+++ sql_injection/mysql_data/00_create_table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS books;
+
+USE books;
+
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    genre VARCHAR(100),
+    year INT
+);

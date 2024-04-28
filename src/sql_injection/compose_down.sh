70ad12fd33155e9b91e905c58c1ee41bf984adb3
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-28 05:37:05 +0300

    no commit message given

--- src/sql_injection/compose_down.sh
+++ src/sql_injection/compose_down.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 # shellcheck source=compose/exercises/nodejs_postgresql/.env
 source .env
-docker-compose down
+docker-compose down --rmi local --volumes --remove-orphans
 # docker volume rm nodejs_postgresql_postgres_data

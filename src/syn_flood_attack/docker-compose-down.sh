1b0910ec5c3ba0fcfeaeb45fe7ef1726b664c1c6
Mark Veltzer <mark.veltzer@gmail.com>
2024-05-05 16:15:05 +0300

    no commit message given

--- src/syn_flood_attack/docker-compose-down.sh
+++ src/syn_flood_attack/docker-compose-down.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -e
-docker-compose down --rmi local --volumes --remove-orphans
+# docker-compose down --rmi local --volumes --remove-orphans
+# docker-compose down --rmi all --volumes --remove-orphans
+docker-compose down

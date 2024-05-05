9c51985613f350c85608c0adc95c07fdef1ea828
Mark Veltzer <mark.veltzer@gmail.com>
2024-05-05 18:26:45 +0300

    no commit message given

--- src/syn_flood_attack/docker-run.sh
+++ src/syn_flood_attack/docker-run.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -e
 docker kill server client hacker || true
+docker network rm flood
+docker network create\
+	--attachable\
+	--subnet=172.28.0.0/16\
+	--ip-range=172.28.5.0/24\
+	--gateway=172.28.5.254\
+	flood
+
 docker run -tid\
 	--rm\
 	--name server\
-	--sysctl 'net.ipv4.tcp_syncookies=0'\
+	--sysctl "net.ipv4.tcp_syncookies=0"\
+	--network flood\
 	syn_flood_attack_server
 docker run -tid\
 	--rm\
 	--name client\
+	--network flood\
 	syn_flood_attack_client
 docker run -tid\
 	--rm\
 	--name hacker\
+	--network flood\
 	syn_flood_attack_hacker

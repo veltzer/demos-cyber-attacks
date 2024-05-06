31a54e1494ee5c7a4fff11427987c52b62058f55
Mark Veltzer <mark.veltzer@gmail.com>
2024-05-06 06:06:38 +0300

    no commit message given

--- src/syn_flood/docker-run.sh
+++ src/syn_flood/docker-run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -e
 docker kill server client hacker || true
 docker network rm flood || true
-docker network create\
-	--attachable\
-	--subnet=172.28.0.0/16\
-	--ip-range=172.28.5.0/24\
-	--gateway=172.28.5.254\
-	flood
+docker network create flood
 
 docker run -tid\
 	--rm\
@@ -23,7 +18,4 @@ docker run -tid\
 	--rm\
 	--name hacker\
 	--network flood\
-	--cap-add=NET_RAW\
-	--cap-add=SYS_ADMIN\
-	--privileged\
 	hacker

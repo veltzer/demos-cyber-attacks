ffe52ec83971720a803bfb97635346dcf9464987
Mark Veltzer <mark.veltzer@gmail.com>
2024-05-26 07:42:32 +0300

    no commit message given

---
+++ src/xss/app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""
+Web server that can add two numbers
+"""
+
+
+import sys
+import requests
+import flask
+
+
+FORM = """
+<html>
+    <body>
+        <form action="/add" method="post">
+            <label for="value">value:</label>
+            <textarea id="value" name="value" rows="10" cols="60"></textarea>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
+"""
+
+ROOT = """
+<!DOCTYPE html>
+<html>
+	<body>
+        Welcome to our chat!</br>
+		<a href="comment">Add a comment</a>
+		<a href="view">View Chat</a>
+	</body>
+</html>
+"""
+
+
+app = flask.Flask("app")
+
+
+@app.route("/")
+def root():
+    """ root url """
+    return ROOT
+
+
+@app.route("/comment")
+def comment():
+    """ comment url """
+    return FORM
+
+
+@app.route("/view")
+def view():
+    """ view url """
+    val = "<html><body>This is the view:"
+    with open("chat.txt", "rt") as stream:
+        val += stream.read()
+    val += "</body></html>"
+    return val
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    """ this will add two numbers given to it """
+    value = flask.request.form.get("value")
+    with open("chat.txt", "at") as stream:
+        stream.write("<br/>")
+        stream.write(value)
+        stream.write("<br/>")
+    return flask.redirect("/")
+
+
+app.run(port=8080, host="0.0.0.0")

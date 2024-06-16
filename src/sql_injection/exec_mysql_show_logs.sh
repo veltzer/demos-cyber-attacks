b469ce9bbe6c9cccac06a876ca41d55162b522b6
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-16 13:19:55 +0300

    no commit message given

--- src/sql_injection/exec_mysql_show_logs.sh
+++ src/sql_injection/exec_mysql_show_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker-compose exec mysql tail -f /var/log/mysql.log
+docker compose exec mysql tail -f /var/log/mysql.log
--- src/sql_injection/logs_mysql.sh
+++ src/sql_injection/logs_mysql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker-compose logs -f mysql
+docker compose logs -f mysql

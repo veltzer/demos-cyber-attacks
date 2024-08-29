533d0616fa7f50f1a34450eb8812eddbac275cf3
Mark Veltzer <mark.veltzer@gmail.com>
2024-08-29 22:14:42 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -16,3 +16,12 @@ VERSION_ID = desktop["VERSION_ID"]
 if VERSION_ID == "24.04":
     # the right package for docker compose
     packages.append("docker-compose-v2")
+
+packages_remove = []
+
+if VERSION_ID == "22.04":
+    # because of conflict with containerd which is needed by "docker.io" below
+    packages_remove.append("containerd.io")
+if VERSION_ID == "24.04":
+    # because of conflict with containerd which is needed by "docker.io" below
+    packages_remove.append("containerd.io")
